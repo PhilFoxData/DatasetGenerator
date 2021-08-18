f9191d0a74e8b3683d08495a270ed5931272cd38
fastFox2 <tamo.fischer@gmail.com>
2021-08-18 14:31:02 +0200

    Added  Support for Dark Theme and Setting so the user can decide if he wants to use light, dark or the system theme.

---
+++ DatasetGenerator/ThemeHelper.cs
@@ -0,0 +1,157 @@
+﻿using Windows.Storage;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace DatasetGenerator
+{
+    public static class ThemeHelper
+    {
+        public static readonly string SelectedAppThemeKey = "SelectedAppTheme";
+        private static Window CurrentApplicationWindow;
+        // Keep reference so it does not get optimized/garbage collected
+        private static UISettings uiSettings;
+        /// <summary>
+        /// Gets the current actual theme of the app based on the requested theme of the
+        /// root element, or if that value is Default, the requested theme of the Application.
+        /// </summary>
+        public static ElementTheme ActualTheme
+        {
+            get
+            {
+                if (Window.Current.Content is FrameworkElement rootElement)
+                {
+                    if (rootElement.RequestedTheme != ElementTheme.Default)
+                    {
+                        return rootElement.RequestedTheme;
+                    }
+                }
+
+                return App.GetEnum<ElementTheme>(App.Current.RequestedTheme.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets (with LocalSettings persistence) the RequestedTheme of the root element.
+        /// </summary>
+        public static ElementTheme RootTheme
+        {
+            get
+            {
+                if (Window.Current.Content is FrameworkElement rootElement)
+                {
+                    return rootElement.RequestedTheme;
+                }
+
+                return ElementTheme.Default;
+            }
+            set
+            {
+                if (Window.Current.Content is FrameworkElement rootElement)
+                {
+                    rootElement.RequestedTheme = value;
+                }
+
+                ApplicationData.Current.LocalSettings.Values[SelectedAppThemeKey] = value.ToString();
+                UpdateSystemCaptionButtonColors();
+
+
+                ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+                if (ThemeHelper.IsDarkTheme())
+                {
+                    titleBar.BackgroundColor = Colors.Black;
+                    titleBar.ForegroundColor = Colors.White;
+
+                    titleBar.InactiveBackgroundColor = (Application.Current.Resources["Scb_InactiveTitlebar"] as SolidColorBrush).Color;
+                    titleBar.InactiveForegroundColor = Colors.Gray;
+
+                    titleBar.ButtonBackgroundColor = Colors.Black;
+                    titleBar.ButtonForegroundColor = Colors.White;
+
+                    titleBar.ButtonInactiveBackgroundColor = (Application.Current.Resources["Scb_InactiveTitlebar"] as SolidColorBrush).Color;
+                    titleBar.ButtonInactiveForegroundColor = Colors.Gray;
+
+                    titleBar.ButtonHoverBackgroundColor = (Application.Current.Resources["Scb_SecondColor"] as SolidColorBrush).Color;
+                }
+                else
+                {
+                    titleBar.BackgroundColor = Colors.White;
+                    titleBar.ForegroundColor = Colors.Black;
+
+                    titleBar.ButtonBackgroundColor = Colors.White;
+                    titleBar.ButtonForegroundColor = Colors.Black;
+
+                    titleBar.InactiveBackgroundColor = Colors.White;
+                    titleBar.InactiveForegroundColor = Colors.White;
+
+                    titleBar.ButtonInactiveBackgroundColor = Colors.White;
+                    titleBar.ButtonInactiveForegroundColor = Colors.White;
+                }
+            }
+        }
+
+        public static void LoadThemeFromSettings()
+        {
+            string savedTheme = ApplicationData.Current.LocalSettings.Values[SelectedAppThemeKey] as string;
+
+            if (savedTheme != null) 
+            {
+                RootTheme =  App.GetEnum<ElementTheme>(savedTheme);
+            }
+        }
+
+        public static void Initialize()
+        {
+            // Save reference as this might be null when the user is in another app
+            CurrentApplicationWindow = Window.Current;
+            string savedTheme = ApplicationData.Current.LocalSettings.Values[SelectedAppThemeKey]?.ToString();
+
+            if (savedTheme != null)
+            {
+                RootTheme = App.GetEnum<ElementTheme>(savedTheme);
+            }
+
+            // Registering to color changes, thus we notice when user changes theme system wide
+            uiSettings = new UISettings();
+            uiSettings.ColorValuesChanged += UiSettings_ColorValuesChanged;
+        }
+
+        private static void UiSettings_ColorValuesChanged(UISettings sender, object args)
+        {
+            // Make sure we have a reference to our window so we dispatch a UI change
+            if (CurrentApplicationWindow != null)
+            {
+                // Dispatch on UI thread so that we have a current appbar to access and change
+                CurrentApplicationWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                        {
+                            UpdateSystemCaptionButtonColors();
+                        });
+            }
+        }
+
+        public static bool IsDarkTheme()
+        {
+            if (RootTheme == ElementTheme.Default)
+            {
+                return Application.Current.RequestedTheme == ApplicationTheme.Dark;
+            }
+            return RootTheme == ElementTheme.Dark;
+        }
+
+        public static void UpdateSystemCaptionButtonColors()
+        {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            if (ThemeHelper.IsDarkTheme())
+            {
+                titleBar.ButtonForegroundColor = Colors.White;
+            }
+            else
+            {
+                titleBar.ButtonForegroundColor = Colors.Black;
+            }
+        }
+    }
+}
